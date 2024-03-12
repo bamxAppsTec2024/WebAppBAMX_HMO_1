d335eab3fe2b7dd1d230afbadc85b49d14f49fab
Fernando Mendivil Terminel <fermendivilt@gmail.com>
2024-03-12 16:15:39 -0700

    Update Admin.jsx

--- src/pages/Cimientos para el futuro/Admin/Admin.jsx
+++ src/pages/Cimientos para el futuro/Admin/Admin.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import app from "../../../firebase";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import Appbar from "../../../components/Cimientos para el futuro/Layout/Appbar";
 
 export default function Admin() {
   const auth = getAuth(app);
@@ -44,48 +45,7 @@ export default function Admin() {
             Buscar
           </button>
         </form>
-        {data.map((category, indexCategory) => (
-          <div className="my-4" key={`category${indexCategory}`}>
-            <h2 role="h1" className="text-success">
-              {category.title}
-            </h2>
-            <div
-              className="d-flex flex-row flex-wrap"
-              style={{ width: "fit-content" }}
-            >
-              {category.courses.map((course, indexCourse) => (
-                <div
-                  key={`course${indexCourse}`}
-                  className="card border-success mb-3 me-5"
-                  style={{ width: "25vw" }}
-                >
-                  <div className="card-header">Taller {indexCourse + 1}</div>
-                  <div className="card-body flex-grow-0">
-                    <h4 className="card-title">{course.title}</h4>
-                    <p className="card-subtitle text-muted">{course.desc}</p>
-                  </div>
-                  <ul className="list-group list-group-flush">
-                    <AccordionItems videos={course.videos} />
-                    {/* {course.videos.map((video, indexVideo) => (
-                      <>
-                        <li
-                          key={`${video.title + indexVideo}`}
-                          className="list-group-item"
-                        >
-                          {video.title}
-                        </li>
-                        <AccordionItem title={video.title} uri={video.youTubeId} />
-                        <button type="button" class="btn btn-success w-1/2">
-                          Success
-                        </button>
-                      </>
-                    ))} */}
-                  </ul>
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
+        
       </main>
     </div>
   );
